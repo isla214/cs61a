b394054c98096fa949da46304ed8670e6dc6332d
Isla <109188673+isla214@users.noreply.github.com>
2022-12-05 21:48:07 -0500

    Add files via upload

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,68 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    result = 1
+    if k > 0:
+        for i in range(0,k):
+            result = result*(n-i) 
+        return result
+    else:
+        return 1
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    result = 0
+    while y > 0:
+        result, y = result + y % 10,  y//10
+    return result
+
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+    while n > 10:# n = 2889
+        x1 = n % 10#9
+        n = n // 10#n = 288
+        x2 = n % 10#8
+        if x1 == x2 == 8:
+            return True
+    return False
+
