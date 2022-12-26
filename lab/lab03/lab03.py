00a8f6e5f4dc491ab091e42cbb6674d43caf20c1
chenxin-May <chenxinzhang214@gmail.com>
2022-12-25 21:27:54 -0500

    lab04

--- lab/lab03/lab03.py
+++ lab/lab03/lab03.py
@@ -193,11 +193,13 @@ def successor(n):
 def one(f):
     """Church numeral 1: same as successor(zero)"""
     "*** YOUR CODE HERE ***"
+    return lambda x: f(x)
 
 
 def two(f):
     """Church numeral 2: same as successor(successor(zero))"""
     "*** YOUR CODE HERE ***"
+    return lambda x: f(f(x))
 
 
 three = successor(two)
@@ -216,6 +218,7 @@ def church_to_int(n):
     3
     """
     "*** YOUR CODE HERE ***"
+    return n(lambda x: x + 1)(0)
 
 
 def add_church(m, n):
@@ -225,6 +228,7 @@ def add_church(m, n):
     5
     """
     "*** YOUR CODE HERE ***"
+    return lambda f: lambda x: m(f)(n(f)(x))
 
 
 def mul_church(m, n):
@@ -237,7 +241,7 @@ def mul_church(m, n):
     12
     """
     "*** YOUR CODE HERE ***"
-
+    return lambda f: m(n(f))
 
 def pow_church(m, n):
     """Return the Church numeral m ** n, for Church numerals m and n.
@@ -248,3 +252,6 @@ def pow_church(m, n):
     9
     """
     "*** YOUR CODE HERE ***"
+    return n(m)
+
+#Q5 and Q6 are diffucult, I cannot understand!!!
