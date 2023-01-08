bc35b2fd87eb08311a5b7ce1b6f79d57692f2b8e
chenxin-May <chenxinzhang214@gmail.com>
2023-01-08 16:14:20 -0500

    finish Lab05 Q89

--- lab/lab05/lab05.py
+++ lab/lab05/lab05.py
@@ -226,9 +226,6 @@ def sprout_leaves(t, leaves):
     return tree(label(t), [sprout_leaves(child, leaves) for child in branches(t)])
 
 
-
-
-
 # Abstraction tests for sprout_leaves and berry_finder
 
 
@@ -291,6 +288,12 @@ def preorder(t):
     [2, 4, 6]
     """
     "*** YOUR CODE HERE ***"
+    def helper(tr, list):
+      list.append(label(tr))
+      for branch in branches(tr):
+        helper(branch, list)
+      return list
+    return helper(t, [])
 
 
 def add_trees(t1, t2):
@@ -329,6 +332,18 @@ def add_trees(t1, t2):
       5
     """
     "*** YOUR CODE HERE ***"
+    if is_leaf(t1) or is_leaf(t2):
+      return tree(label(t1) + label(t2), branches(t1) + branches(t2))
+    new_branches = []
+    len1, len2 = len(branches(t1)), len(branches(t2))
+    for i in range(min(len1, len2)):
+        new_branches += [add_trees(branches(t1)[i], branches(t2)[i])]
+    for i in range(min(len1,len2),max(len1,len2)):
+        if len1>len2:
+            new_branches += [branches(t1)[i]]
+        else:
+            new_branches += [branches(t2)[i]]
+    return tree(label(t1) + label(t2), new_branches)
 
 
 def change_abstraction(change):
